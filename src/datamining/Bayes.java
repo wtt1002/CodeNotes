7e90525c47d989e0f2c11fac77d8ee3d8fc2acf4
wtt1002 <wtt1002@qq.com>
2018-06-25 22:05:26 +0800

    朴素贝叶斯

---
+++ src/datamining/Bayes.java
@@ -0,0 +1,110 @@
+package datamining;
+
+import javax.transaction.xa.Xid;
+
+/**
+ * Package: datamining
+ * Description： TODO
+ * Author: TingTing W
+ * Date: Created in 2018/6/25 20:44
+ */
+public class Bayes {
+    static int [][] data = {
+                     {0,1,3,2,2,2},
+                     {0,1,3,2,1,2},
+                     {0,2,3,2,2,1},
+                     {0,3,2,2,2,1},
+                     {0,3,1,1,2,1},
+                     {0,3,1,1,1,2},
+                     {0,2,1,1,1,1},
+                     {0,1,2,2,2,2},
+                     {0,1,1,1,2,1},
+                     {0,3,2,1,2,1},
+                     {0,1,2,1,1,1},
+                     {0,2,2,2,1,1},
+                     {0,2,3,1,2,1},
+                     {0,3,2,2,1,2},};
+
+    public static double bothInfo(int xIndex, int xRange, int yIndex,int yRange, int zIndex, int zRange){
+        double outCome = 0;
+        double prob = 0;
+        double value= 0;
+        double valOne = 0;
+        double valTwo = 0;
+        for (int i = 1; i <= xRange; i++){
+            for (int j = 1; j <= yRange; j++){
+                for (int k = 1; k <= zRange; k++) {
+                    prob = getProb_xyz(i,xIndex,j,yIndex,k,zIndex);
+                    value = getProb_xyByz(i,xIndex,j,yIndex,k,zIndex);
+                    valOne = getProb_aByb(i, xIndex, k, zIndex);
+                    valTwo = getProb_aByb(j, xIndex, k, zIndex);
+                    if (prob - 0 < 0.0000000001)continue;
+                    if (value - 0 < 0.0000000001 )continue;
+                    if (valOne - 0 < 0.0000000001)continue;;
+                    if (valTwo - 0 < 0.0000000001)continue;
+                    outCome += calcute(prob,value,valOne,valTwo);
+                    System.out.println("======outCome:"+outCome);
+                }
+            }
+        }
+
+        System.out.println("最终："+outCome);
+        return outCome;
+    }
+
+    private static double calcute(double prob, double value, double valOne, double valTwo) {
+        double upValue = value/(valOne * valTwo);
+        double calValue = Math.log(upValue)/Math.log(2);
+        System.out.println("upValue:" + upValue + "  calValue:" + calValue);
+        System.out.println("calcute:"+prob * calValue);
+        return prob * calValue ;
+    }
+
+    private static double getProb_xyz(int x, int xIndex, int y, int yIndex, int z, int zIndex){
+        double countXYZ= 0;
+        for (int i = 0; i < data.length; i++){
+            if (data[i][zIndex] == z && data[i][xIndex] == x && data[i][yIndex] == y){
+                countXYZ++;
+            }
+        }
+        System.out.println("countXYZ:" + countXYZ + "  data.length:" + data.length);
+        System.out.println("getProb_xyz:" + countXYZ/data.length);
+        return countXYZ/data.length;
+    }
+
+    private static double getProb_xyByz(int x, int xIndex, int y, int yIndex, int z, int zIndex){
+        double countZ = 0;
+        double countXY_Z = 0;
+        for (int i = 0; i < data.length; i++){
+            if (data[i][zIndex] == z){
+                countZ++;
+                if (data[i][xIndex] == x && data[i][yIndex] == y){
+                    countXY_Z++;
+                }
+            }
+        }
+        System.out.println("countXY_Z:" + countXY_Z + "  countZ:" + countZ);
+        System.out.println("getProb_xyByz:" + countXY_Z/countZ);
+        return countXY_Z/countZ;
+    }
+
+    private static double getProb_aByb(int a, int aIndex, int b, int bIndex) {
+        double countB = 0;
+        double countA_B = 0;
+        for (int i = 0; i < data.length; i++){
+            if (data[i][bIndex] == b){
+                countB++;
+                if (data[i][aIndex] == a){
+                    countA_B++;
+                }
+            }
+        }
+        System.out.println("countA_B:" + countA_B + "  countB:" + countB);
+        System.out.println("getProb_aByb:"+countA_B/countB);
+        return countA_B/countB;
+    }
+
+    public static void main(String[] args) {
+        bothInfo(1,3,2,3,5,2);
+    }
+}
