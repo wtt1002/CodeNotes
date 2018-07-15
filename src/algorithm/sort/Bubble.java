7b273ac1ea472e63b362f7a4dc81e5ca7f532d90
wtt1002 <wtt1002@qq.com>
2018-07-15 12:35:02 +0800

    排序算法集合

---
+++ src/algorithm/sort/Bubble.java
@@ -0,0 +1,31 @@
+package algorithm.sort;
+
+/**
+ * Package: algorithm.sort
+ * Description： 冒泡排序
+ * Author: TingTing W
+ * Date: Created in 2018/7/15 11:14
+ */
+public class Bubble {
+    public void bubbleSort(int [] array)
+    {
+        if (array == null || array.length == 0) {
+            return;
+        }
+        int temp;
+        int length = array.length;
+        for (int i = 0; i < length; i++){
+            for (int j = length - 1; j > i; j--){
+                if (array[j] < array[j - 1]){
+                    temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                }
+            }
+        }
+
+        for (int num : array) {
+            System.out.println(num);
+        }
+    }
+}
