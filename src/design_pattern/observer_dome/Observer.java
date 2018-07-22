b703bb7eb5ec488b6ecf815e327d75a4c5ee213a
wtt1002 <wtt1002@qq.com>
2018-07-22 15:18:31 +0800

    设计模式

---
+++ src/design_pattern/observer_dome/Observer.java
@@ -0,0 +1,11 @@
+package design_pattern.observer_dome;
+
+/**
+ * Package: design_pattern.observer_dome
+ * Description： TODO
+ * Author: TingTing W
+ * Date: Created in 2018/7/22 11:32
+ */
+public interface Observer {
+    public void update(float temp, float humidity, float pressure);
+}
