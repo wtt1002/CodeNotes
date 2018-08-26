ec50d12a9364034ca428663c952a7d0741ca6b5a
wtt1002 <wtt1002@qq.com>
2018-08-26 15:03:30 +0800

    今日头条2-第四题

---
+++ src/qiuzhao/jinritoutiao2/question_4.java
@@ -0,0 +1,18 @@
+package qiuzhao.jinritoutiao2;
+
+import java.util.Scanner;
+
+/**
+ * 未完成
+ */
+public class question_4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int len = scanner.nextInt();
+        int hours = scanner.nextInt();
+        int[] array = new int[len];
+        for (int i = 0; i < len; i++){
+            
+        }
+    }
+}
