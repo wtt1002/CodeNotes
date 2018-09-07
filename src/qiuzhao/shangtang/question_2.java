cc39555ec963a130d985a68e3654fdee2023ff8c
wtt1002 <wtt1002@qq.com>
2018-09-07 21:59:53 +0800

    商汤科技第二题

---
+++ src/qiuzhao/shangtang/question_2.java
@@ -0,0 +1,15 @@
+package qiuzhao.shangtang;
+
+import java.util.Scanner;
+
+/**
+ * Package: qiuzhao.shangtang
+ * Description： TODO
+ * Author: TingTing W
+ * Date: Created in 2018/9/7 18:40
+ */
+public class question_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
