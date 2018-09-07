b7d00550977443cf16ff8f335b7901b15f765a7e
wtt1002 <wtt1002@qq.com>
2018-09-07 21:59:31 +0800

    商汤科技第一题

---
+++ src/qiuzhao/shangtang/question_1.java
@@ -0,0 +1,15 @@
+package qiuzhao.shangtang;
+
+import java.util.Scanner;
+
+/**
+ * Package: qiuzhao.shangtang
+ * Description： TODO
+ * Author: TingTing W
+ * Date: Created in 2018/9/7 18:39
+ */
+public class question_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+    }
+}
