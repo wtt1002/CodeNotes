6362e5f3267ce0a20ae50260967704af2d3d0b01
wtt1002 <wtt1002@qq.com>
2018-09-08 09:18:06 +0800

    商汤科技第二题

---
+++ src/qiuzhao/shangtang/question_2.java
@@ -0,0 +1,29 @@
+package qiuzhao.shangtang;
+
+import java.util.Scanner;
+
+/**
+ * Package: qiuzhao.shangtang
+ * Description： TODO
+ * Author: TingTing W
+ * Date: Created in 2018/9/7 0007$ 下午 6:53$
+ */
+public class question_2 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = scanner.nextInt();
+        for (int i = 0; i < num; i++){
+            int raw = scanner.nextInt();
+            int col = scanner.nextInt();
+            for (int j = 0; j < raw; j++){
+                for (int k = 0; k < col; k++){
+                    scanner.next();
+                }
+
+            }
+        }
+        for (int i = 0; i < num; i++){
+            System.out.println(true);
+        }
+    }
+}
