167faef8e5ae625c8bb65c4b22557c0afe075105
wtt1002 <wtt1002@qq.com>
2018-09-26 09:01:03 +0800

    商汤-第一题-new

---
+++ src/qiuzhao/shangtang/question_1_new.java
@@ -0,0 +1,13 @@
+package qiuzhao.shangtang;
+
+/**
+ * Package: qiuzhao.shangtang
+ * Description： TODO
+ * Author: TingTing W
+ * Date: Created in 2018/9/8 9:56
+ */
+public class question_1_new {
+    public static void main(String[] args) {
+
+    }
+}
